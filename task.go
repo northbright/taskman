d1a90e1714c4f944147b4a76ff3112fd4d9a5527
Frank Xu <frank.xu@northbright.com>
2021-07-20 13:47:51 +0800

    Add more comments for Step and remove context parameter

--- task.go
+++ task.go
@@ -1,19 +1,22 @@
 package taskman
 
 import (
-	"context"
 	"encoding"
 )
 
 type Task interface {
 	encoding.BinaryMarshaler
 	encoding.BinaryUnmarshaler
-	Init(ctx context.Context) (int64, error)
-	Deinit(ctx context.Context) error
-	// Return values:
-	// int64: next step index
-	// bool: if task is done
-	// []byte: result when it's done
-	// error
-	Step(ctx context.Context, current int64) (int64, bool, []byte, error)
+	Init() (int64, error)
+	Deinit() error
+	// Step does the real work.
+	// A large task can be devided into many small pieces(steps).
+	// Param:
+	// current: current step(index) to do.
+	// Return:
+	// int64: next step(index) to do.
+	// bool: if task is done. The task goroutine will exit when it's true.
+	// []byte: result when it's done otherwise it's ignored.
+	// error: any error in the step will stop the task.
+	Step(current int64) (int64, bool, []byte, error)
 }
